aed44524f6947fe0828ca08e269efbeee0dcb42b
edualt <eduardosebastianhdz503@gmail.com>
2024-06-06 01:18:38 -0600

    test

--- test.js
+++ test.js
@@ -3,14 +3,14 @@ const app = express();
 const request = require('supertest');
 
 app.get('/api/v1/welcome', (req, res) => {
-  res.send('Hello, World2!');
+  res.send('Hello, World3!');
 });
 
 describe('GET /api/v1/welcome', () => {
-  it('respond with Hello, World2!', (done) => {
+  it('respond with Hello, World3!', (done) => {
     request(app)
       .get('/api/v1/welcome')
-      .expect('Hello, World2!', done);
+      .expect('Hello, World3!', done);
   });
 });
 
